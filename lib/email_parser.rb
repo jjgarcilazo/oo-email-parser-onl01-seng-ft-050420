abe99987b566a4c5fe048abcb68263daea0cfa51
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-11 20:44:40 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,15 +4,16 @@
 # or whitespace (' ').
 class EmailParser
 
-  attr_accessor :email
-  def  initialize(emails)
-    @email = emails
+  attr_reader :addresses_raw
+
+  def initialize(addresses_string)
+    @addresses_raw = addresses_string
   end
 
   def parse
-    email_array = @email.split(/[, ]/).uniq
-    email_array.reject! {|element| element.empty?}
-    email_array
+  
+    addresses_raw.split(",").join.split.uniq
+  
   end
 
 end
